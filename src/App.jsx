73e5383208303617f8322d91c16a736fa0d87b36
alyza shavers <shavers.alyza@gmail.com>
2024-01-27 12:42:51 -0800

    Add styles to the React component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { puppyList } from './data.js';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const [count, setCount] = useState (0);
+
+
+  const handleClick = (puppyId) => {
+    setFeatPupId(puppyId);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log("Featured Puppy:", featuredPup);
 
   return (
     <>
@@ -17,7 +30,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="puppy-list">
+        {puppies.map((puppy) => (
+          <p key={puppy.id} onClick={() => handleClick(puppy.id)}>
+            {puppy.name}
+            </p>
+        ))}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,11 +43,21 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      {featuredPup && (
+      <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+      )}
+      <div className="my-styled-div">Hello, World!</div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
 export default App
